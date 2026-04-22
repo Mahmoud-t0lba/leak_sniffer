e823e3bd1f260b04b3d2c28b1a57d39f7307005b
Mahmoud Tolba <78425511+Mahmoud-t0lba@users.noreply.github.com>
2026-04-22 08:28:42 +0200

    remove several test files

--- packages/leak_sniffer/test/fixtures/stream_controller_fix_create_dispose.dart
+++
@@ -1,7 +0,0 @@
-// ignore_for_file: unused_field
-
-import 'dart:async';
-
-class StreamOwner {
-  final StreamController<int> _controller = StreamController<int>();
-}
--- packages/leak_sniffer/test/fixtures/timer_fix_existing_dispose.dart
+++
@@ -1,9 +0,0 @@
-// ignore_for_file: unused_field
-
-import 'dart:async';
-
-class TimerOwner {
-  final Timer _timer = Timer.periodic(const Duration(seconds: 1), (_) {});
-
-  void dispose() {}
-}
